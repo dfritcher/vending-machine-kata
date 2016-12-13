ce39e1d55cc7a9f7bb8ca246722cc9eef1051d35
David Fritcher <dfritcher@gmail.com>
2016-12-12 21:12:14 -0500

    Create enumerator for coin

---
+++ VendingMachine/VendingMachine/Enumerators.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Enumerators
+    {
+        enum Size { NineteenMM, TwentyOneMM, SeventeenMM, TwentyFourMM };
+
+        enum Weight { TwoAndAHalfGrams, FiveGrams,  TwoGrams, FiveAndAHalfGrams }
+    }
+}
