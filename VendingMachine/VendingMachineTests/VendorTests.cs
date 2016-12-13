9a67b2c473ea3b46e9e46533f6749201cb54d5d5
David Fritcher <dfritcher@gmail.com>
2016-12-12 20:36:51 -0500

    Setup main project and test project

---
+++ VendingMachine/VendingMachineTests/VendorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class VendorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
